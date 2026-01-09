68e6b1a9362304df3ced9b6ec69e5a1f3adcc152
Cirgle01 <Cirgle@outlook.com>
2026-01-09 15:12:59 +0800

    代码缩进修复

--- util.c
+++ util.c
@@ -14,26 +14,26 @@ void error(char *fmt, ...) {
 
 // 读取并返回指定文件的内容
 char *read_file(char *path) {
-  // 打开文件
-  FILE *fp = fopen(path, "r");
-  if (!fp)
-    error("cannot open %s: %s", path, strerror(errno));
+    // 打开文件
+    FILE *fp = fopen(path, "r");
+    if (!fp)
+        error("cannot open %s: %s", path, strerror(errno));
 
-  // 获取文件长度
-  if (fseek(fp, 0, SEEK_END) == -1)
-    error("%s: fseek: %s", path, strerror(errno));
-  size_t size = ftell(fp);
-  if (fseek(fp, 0, SEEK_SET) == -1)
-    error("%s: fseek: %s", path, strerror(errno));
+    // 获取文件长度
+    if (fseek(fp, 0, SEEK_END) == -1)
+        error("%s: fseek: %s", path, strerror(errno));
+    size_t size = ftell(fp);
+    if (fseek(fp, 0, SEEK_SET) == -1)
+        error("%s: fseek: %s", path, strerror(errno));
 
-  // 读取文件内容
-  char *buf = calloc(1, size + 2);
-  fread(buf, size, 1, fp);
+    // 读取文件内容
+    char *buf = calloc(1, size + 2);
+    fread(buf, size, 1, fp);
 
-  // 确保文件以"\n\0"结尾(待扩展)
-  if (size == 0 || buf[size - 1] != '\n')
-    buf[size++] = '\n';
-  buf[size] = '\0';
-  fclose(fp);
-  return buf;
+    // 确保文件以"\n\0"结尾(待扩展)
+    if (size == 0 || buf[size - 1] != '\n')
+        buf[size++] = '\n';
+    buf[size] = '\0';
+    fclose(fp);
+    return buf;
 }
